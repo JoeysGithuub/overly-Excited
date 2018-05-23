a2867cb40b29351d1a1979380f7ce03cfb315748
Joey <smithjoey5123@gmail.com>
2018-05-23 15:32:21 -0500

    New stuff

--- apps.js
+++ apps.js
@@ -1,23 +1,28 @@
 let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
-
+let nextSentence = ["I", "am", "so", "terrible", "at", "coding"];
 /*
     The addExcitement function should be an impure function, and accept
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement (theWordArray) {
-
+let addExcitement = (theWordArray,) => {
+    let point = '!';
+    let question = '?';
     // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
 
-    for (let i = 0; i < theWordArray.length; i++) {
+    for (let i = 1; i < theWordArray.length; i++) {
         // Concatenate the new word onto buildMeUp
-        buildMeUp += theWordArray[i]
+        buildMeUp += ` ${theWordArray[i-1]}`
         // Print buildMeUp to the console
         console.log(buildMeUp)
+        if (i % 3 == 0) {
+            buildMeUp += point;
+            point += '!';
+
     }
 
 }
-
+}
 // Invoke the function and pass in the array
-addExcitement(sentence)
+addExcitement(nextSentence, "&");
